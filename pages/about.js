ca7ff0f9947aac9c090252d262dd30df95bb164c
Ana Brandão <anabrandao@Anas-MacBook-Pro.local>
2020-03-12 15:35:49 +0100

    add about page content

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ export default function About () {
     return (
       <Layout>     
         <h2> About </h2>
-        <div>Add some content here</div>
+        <div>This is a pokedex. All the info used come from <strong><a href="https://pokeapi.co/" target="_blank">pokeapi</a></strong></div>
       </Layout>
     )
 } 
