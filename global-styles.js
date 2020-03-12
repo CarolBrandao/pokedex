66233429f9e4393e4c40134f3b9199cfb027b868
Ana Brandão <anabrandao@Anas-MacBook-Pro.local>
2020-03-12 15:47:42 +0100

    delete unnecessary react import

--- global-styles.js
+++ global-styles.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useTheme } from 'emotion-theming'
 
 import styledSystemCss from '@styled-system/css'
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import styled from '@emotion/styled'
 
 import Layout from '../components/layout'
